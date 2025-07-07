f324ed979bf9e75fd03f77da5de804a327b24ce3
myrroearl <orymsazellim@gmail.com>
2025-07-07 20:14:28 +0800

    Update 2025_06_29_122213_add_board_passer_columns_to_users_table.php

--- database/migrations/2025_06_29_122213_add_board_passer_columns_to_users_table.php
+++ database/migrations/2025_06_29_122213_add_board_passer_columns_to_users_table.php
@@ -12,10 +12,12 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_board_passer')->nullable()->default(null)->after('leadership');
-            $table->string('board_exam_name', 150)->nullable()->after('is_board_passer');
-            $table->integer('board_exam_year')->nullable()->after('board_exam_name');
-            $table->string('license_number', 100)->nullable()->after('board_exam_year');
+            if (!Schema::hasColumn('users', 'is_board_passer')) {
+                $table->boolean('is_board_passer')->nullable()->default(null)->after('leadership');
+                $table->string('board_exam_name', 150)->nullable()->after('is_board_passer');
+                $table->integer('board_exam_year')->nullable()->after('board_exam_name');
+                $table->string('license_number', 100)->nullable()->after('board_exam_year');
+            }
         });
     }
 
