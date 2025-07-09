cd66ca9bcc4086d8895a4d15115e1917ffa904c2
myrroearl <orymsazellim@gmail.com>
2025-07-10 06:43:13 +0800

    Update StudentForecastController.php

--- app/Http/Controllers/Admin/StudentForecastController.php
+++ app/Http/Controllers/Admin/StudentForecastController.php
@@ -10,8 +10,8 @@ class StudentForecastController extends Controller
 {
     public function index()
     {
-        $detailedPredictionsFile = public_path('aseets/predictions/detailed_predictions.txt');
-        $csvPredictionsFile = public_path('aseets/predictions/student_employability_predictions.csv');
+        $detailedPredictionsFile = public_path('assets/predictions/detailed_predictions.txt');
+        $csvPredictionsFile = public_path('assets/predictions/student_employability_predictions.csv');
 
         $detailedPredictions = file_exists($detailedPredictionsFile) ? file_get_contents($detailedPredictionsFile) : '';
         $csvData = [];
