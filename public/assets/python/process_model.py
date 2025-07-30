40d291985abd76a10e2e6677ab71f7a01295323b
myrroearl <orymsazellim@gmail.com>
2025-07-31 02:48:10 +0800

    Update process_model.py

--- public/assets/python/process_model.py
+++ public/assets/python/process_model.py
@@ -565,7 +565,7 @@ try:
             user='root',
             password='uzTWebEtRKIvSROmBYNgcQdrelFjZDgE',
             database='railway',
-            port=48259
+            port='48259'
         )
         cursor = conn.cursor()
         
@@ -608,7 +608,7 @@ try:
         user='root',
         password='uzTWebEtRKIvSROmBYNgcQdrelFjZDgE',
         database='railway',
-        port=48259
+        port='48259'
     )
     cursor = conn.cursor()
 
